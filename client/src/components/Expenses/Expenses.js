58d73e501b50372abe96ea8215fd22bdf01884d5
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-04-22 13:58:52 +0545

    Used use state and worked with conditional statement in the expense handler

--- client/src/components/Expenses/Expenses.js
+++ client/src/components/Expenses/Expenses.js
@@ -1,45 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import ExpensesFilter from './ExpenseFilter';
+import ExpenseFilter from '../Expenses/ExpenseFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] =useState('2020');
-  const filterChangeHandler = selectedYear =>{
-    console.log('Expenses.js');
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+ 
   return (
-    <>
     <div>
-    <Card className="expenses">
-    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
     </div>
-    </>
   );
-}
+};
 
 export default Expenses;
--- client/src/components/NewExpense/ExpenseForm.js
+++ client/src/components/NewExpense/ExpenseForm.js
@@ -46,12 +46,12 @@ const ExpenseForm = (props) => {
   }
 
   const submitHandler = (event)=>{
-    event.preventDefault();
+    event.preventDefault()
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate)
-    }
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
+    };
 
     props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
@@ -75,6 +75,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
